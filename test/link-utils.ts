0549ad5fefd09311541914cd5b7af8110f01b4ad
Kris Read <kris@neofinancial.com>
2020-10-31 12:24:08 -0600

    format

--- test/link-utils.ts
+++ test/link-utils.ts
@@ -1,10 +1,10 @@
-import ResponseLike from "responselike";
-import gql from "graphql-tag";
-import { ApolloLink, execute } from "apollo-link";
+import ResponseLike from 'responselike';
+import gql from 'graphql-tag';
+import { ApolloLink, execute } from 'apollo-link';
 
-import { FetchResult } from "@apollo/client/core";
+import { FetchResult } from '@apollo/client/core';
 
-const responseHeaders =  {};
+const responseHeaders = {};
 const responseObject = { foo: 'bar' };
 
 class MockResponse extends ResponseLike {
@@ -16,17 +16,27 @@ class MockResponse extends ResponseLike {
 type FetchType = (input: RequestInfo, init?: RequestInit) => Promise<Response>;
 type HeadersType = { [header: string]: string | string[] | undefined };
 
-const createMockFetch = (headers: HeadersType = responseHeaders, body: object = responseObject, url?: string): FetchType => {
+const createMockFetch = (
+  headers: HeadersType = responseHeaders,
+  body: object = responseObject,
+  url?: string
+): FetchType => {
   return async (requestInfo: RequestInfo): Promise<Response> => {
-    const uri: string = url || (typeof requestInfo === 'string') ? (requestInfo as string) : (requestInfo as Request)?.url;
+    const uri: string =
+      url || typeof requestInfo === 'string' ? (requestInfo as string) : (requestInfo as Request)?.url;
 
-    return new MockResponse(200, headers, Buffer.from(JSON.stringify(
-      {
-        data: body
-      }
-    )), uri) as unknown as Response
-  }
-}
+    return (new MockResponse(
+      200,
+      headers,
+      Buffer.from(
+        JSON.stringify({
+          data: body
+        })
+      ),
+      uri
+    ) as unknown) as Response;
+  };
+};
 
 const MockQuery = gql`
   query {
@@ -36,14 +46,12 @@ const MockQuery = gql`
 
 const executeRequest = async (link: ApolloLink): Promise<FetchResult> => {
   return new Promise((resolve, reject) => {
-      // Apollo uses Observables. 
-      execute(link, { query: MockQuery }).subscribe(
-        (value) => resolve(value), 
-        (error) => reject(error)
-      );
-  })
-}
+    // Apollo uses Observables.
+    execute(link, { query: MockQuery }).subscribe(
+      value => resolve(value),
+      error => reject(error)
+    );
+  });
+};
 
-export {
-  MockResponse, MockQuery, createMockFetch, executeRequest
-}
+export { MockResponse, MockQuery, createMockFetch, executeRequest };
